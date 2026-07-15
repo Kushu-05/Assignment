4e1c77b4ac6587ea9e67d4ce2cbdaacf77215891
Kushu-05 <kusharitha.gowda@prodapt.com>
2026-07-16 03:04:25 +1300

    Add files via upload

---
+++ Day8_work/ecommerce/test_user_auth.py
@@ -0,0 +1,18 @@
+from user_auth import UserAuth
+auth = UserAuth()
+def test_register_login():
+    assert auth.register("user1","password1")==True
+    assert auth.login("user1","password1")==True
+
+def test_register_existing_user():
+    assert auth.register("admin","newpassword")==False
+
+def test_login_password():
+    auth.register("user2","password2")
+    assert auth.login("user2","wrongpassword")==False
+
+def test_login():
+    assert auth.login("admin","admin123")==True
+
+
+
